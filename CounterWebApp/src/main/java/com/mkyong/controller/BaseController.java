a1f6a745d28a965ac1a024488c6cf2a26b53622f
MindtreeDevOpsDemo <mindtreedevopsdemo@gmail.com>
2015-08-17 13:15:21 +0530

    passing test cases latest

--- CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
+++ CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
@@ -18,7 +18,7 @@ public class BaseController {
 	public String welcome(ModelMap model) {
 
 		model.addAttribute("message", "Welcome");
-		model.addAttribute("counter", "Jun");
+		model.addAttribute("counter", "Raj");
 		logger.debug("[welcome] counter : {}", counter);
 
 		//Spring uses InternalResourceViewResolver and return back index.jsp
