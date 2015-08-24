dd212daf77015bff2386532a82fa72d470564f6e
MindtreeDevOpsDemo <mindtreedevopsdemo@gmail.com>
2015-08-24 15:42:01 +0530

    Jenkins Auto Compilation

--- CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
+++ CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
@@ -18,7 +18,7 @@ public class BaseController {
 	public String welcome(ModelMap model) {
 
 		model.addAttribute("message", "Welcome");
-		model.addAttribute("counter", "Hi Prachi");
+		model.addAttribute("counter", "Hi Raj");
 		logger.debug("[welcome] counter : {}", counter);
 
 		//Spring uses InternalResourceViewResolver and return back index.jsp
